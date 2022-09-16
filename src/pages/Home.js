23adb46b4286524fbd64456fa70d91c9b1ab2073
Vander Lima <vnd.vander@gmail.com>
2022-09-15 23:44:23 -0300

    StyleSheet

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,73 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Platform,
+  TouchableOpacity
+} from "react-native";
 
 export function Home() {
   return (
-    <View>
-      <Text>React Native</Text>
+    <View
+      style={styles.container}
+    >
+      <Text
+        style={styles.title}
+      >
+        Welcome Vander
+      </Text>
+      <TextInput
+        style={styles.input}
+        placeholder={'Nova Habilidade'}
+        placeholderTextColor={'#555'}
+      />
+
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={.7}
+      >
+        <Text style={styles.buttonText}>Adicionar</Text>
+      </TouchableOpacity>
+
+      <Text
+        style={[styles.title, { marginTop: 50 }]}
+      >
+        Minhas Habilidades
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingVertical: 70,
+    paddingHorizontal: 50
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  input: {
+    backgroundColor: '#1f1e25',
+    color: '#fff',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    marginBottom: 20,
+    borderRadius: 8
+  },
+  button: {
+    backgroundColor: '#a370f7',
+    padding: 20,
+    borderRadius: 7,
+    alignItems: 'center'
+  },
+  buttonText: {
+    color: '#fff',
+  }
+})
